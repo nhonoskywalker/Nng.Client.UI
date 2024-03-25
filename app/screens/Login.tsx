a2ba6577c294ae7033e1bc7534ab25d2a5dc800d
ashvinsunga <ashvinsunga@yahoo.com.ph>
2024-03-26 01:09:54 +0800

    'success_page_created'

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,66 +1,83 @@
-import { View, Text, StyleSheet, TextInput, ActivityIndicator, Button, Alert } from 'react-native'
+import { View, Text, StyleSheet, TextInput, SafeAreaView, TouchableOpacity, Alert,Image } from 'react-native'
 import React, { useState } from 'react'
-import { FIREBASE_AUTH } from '../../FirebaseConfig';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const auth = FIREBASE_AUTH
-
-    const signIn = async () => {
-        setLoading(true);
-        try {
-            const response = await signInWithEmailAndPassword(auth, email, password);
-            console.log(response);
-            alert('Logged in Successfully!')
-        } catch (error: any) {
-            console.log(error);
-            alert('Login Failed' + error.message)
-        } finally {
-            setLoading(false)
-        }
-    }
-
-    const signUp = async () => {
-        setLoading(true);
-        try {
-            const response = await createUserWithEmailAndPassword(auth, email, password);
-            console.log(response);
-            alert('Account created sucessfully ')
-        } catch (error:any) {
-            console.log(error);
-            alert('Registration Failed' + error.message)
-        } finally {
-            setLoading(false)
-        }
-    }
+    const [form, setForm] = useState({
+        email:'',
+        password:'', 
+      })
+      return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#e8ecf4'}}>
+        <View style={styles.container}>
+          <View style= {styles.header}>
+            <Image
+              source ={require("../../assets/nonogo.png")}
+              style={styles.headerImage}
+              alt=""
+            />
+        
+            <Text style={styles.title}> Sign in to NonoGo</Text>
+            <Text style={styles.subtitle}> Get access to your daily servicing needs</Text>
+          </View>
     
+          <View>
+            <View style={styles.input}>
+              <Text style={styles.inputLabel}>Email Address</Text>
     
-  return (
-    <View style={styles.container}>
-      <TextInput value={email} style={styles.input} placeholder='Email' autoCapitalize='none' onChangeText={(text) => {
-        setEmail(text)
-      }}>
-      </TextInput>
-
-      <TextInput value={password} secureTextEntry={true} style={styles.input} placeholder='Password' autoCapitalize='none' onChangeText={(text) => {
-        setPassword(text)
-      }}>
-      </TextInput>
-      <Text> </Text>
-      {loading ? <ActivityIndicator size="large" color="#000ff" />
-      : <>
-      <Button title="Login" onPress={signIn} />
-      <Text> </Text>
-      <Button title="Create Account" onPress={signUp} />
-      </>
-      }
-    </View>
-  )
+              <TextInput 
+                autoCapitalize='none'
+                autoCorrect={false}
+                style={styles.inputControl}
+                placeholder='user@example.com'
+                placeholderTextColor="#6b7280"
+                value={form.email}
+                onChangeText={email => setForm( {...form,email})}
+              />
+            </View>
+    
+            <View style={styles.input}>
+              <Text style={styles.inputLabel}>Password</Text>
+    
+              <TextInput
+                secureTextEntry
+                style={styles.inputControl}
+                placeholder='**********'
+                placeholderTextColor="#6b7280"
+                value={form.password}
+                onChangeText={password => setForm( {...form,password})}
+              />
+            </View>
+    
+            <View style={styles.formAction}>
+              <TouchableOpacity onPress={()=>{
+                // handle onPress
+                Alert.alert('Successfully logged in!')
+              }}>
+                <View style={styles.btn}>
+                  <Text style={styles.btnText}>
+                    Sign in
+                  </Text>
+                </View>
+                
+              </TouchableOpacity>
+            </View>
+    
+            <TouchableOpacity
+            style={{marginTop: 'auto'}}
+            onPress={() => {
+              //handle onPress
+            }}>
+              <Text style={styles.formFooter}>
+                Don't have an account? {' '}
+                <Text style={{textDecorationLine: 'underline'}}> Sign up</Text>
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        </SafeAreaView>
+      );
 
 }
 
@@ -68,20 +85,80 @@ export default Login
 
 
 const styles = StyleSheet.create({
-    container: {
-        marginHorizontal: 20,
-        flex: 1,
-        justifyContent: 'center'
-    },
-    input: {
-        marginVertical: 4,
-        height: 50,
-        borderWidth: 1,
-        borderRadius: 4,
-        padding: 10,
-        backgroundColor: 'white'
-    },
-    button: {
-        padding: 2
-    }
+    btn: {
+    backgroundColor: '#075eec',
+    borderRadius: 15,
+    borderWidth: 0.5,
+    borderColor: 'black',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 20
+  },
+  btnText: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#fff'
+  },
+  container: {
+    paddings: 20,
+    flex: 1,
+    margin: 30
+  },
+  form: {
+    marginBottom: 24,
+    flex: 1
+  },
+  formAction: {
+    marginVertical: 24
+  },
+  formFooter: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#222',
+    textAlign: 'center',
+    letterSpacing: 0.25
+  },
+  header: {
+    marginVertical: 40,
+  },
+  headerImage: {
+    width: 150,
+    height: 80,
+    alignSelf: 'center',
+    marginBottom: 30
+  },
+  input: {
+    marginBottom: 10
+  },
+  inputControl: {
+    height: 44,
+    backgroundColor: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#222'
+  },
+  inputLabel: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#222',
+    marginBottom: 9
+  },
+  subtitle:{
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#929292',
+    textAlign: 'center'
+  },
+  title: {
+    fontSize : 30,
+    fontWeight: '800',
+    color: '#1e1e1e',
+    marginBottom: 6,
+    textAlign: 'center'
+  }
 })
--- app/screens/createAccountSuccessPage.tsx
+++ app/screens/createAccountSuccessPage.tsx
@@ -1,12 +1,73 @@
-import { View, Text } from 'react-native'
+import { View, Image, Text,StyleSheet, ImageBackground } from 'react-native'
 import React from 'react'
 
-const createAccountSuccessPage = () => {
+
+const CreateAccountSuccessPage = () => {
   return (
-    <View>
-      <Text>createAccountSuccessPage</Text>
+    <View style={styles.background}>
+      <ImageBackground 
+        style={styles.background}
+        source={require('../../assets/successBg.jpg')}
+      >
+        <View style={styles.successContainer}>
+          <Image 
+          style={styles.successIcon}
+          source={require('../../assets/successIcon.png')}/>
+
+          <View>
+            <Text style={styles.successText}>You have successfully created your account</Text>
+          </View>
+
+          <View style={styles.btn}>
+            <Text style={styles.btnText}> SIGN IN</Text>
+          </View>
+
+        </View>
+
+      </ImageBackground>
+
+
     </View>
+    
   )
 }
 
-export default createAccountSuccessPage
+
+export default CreateAccountSuccessPage
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+  btn: {
+    backgroundColor: 'royalblue',
+    width: "50%",
+    height: 35,
+    borderRadius: 15,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+  },
+  btnText: {
+    fontSize: 18,
+    fontWeight: '400',
+    color: 'white',
+    textAlign:"center"
+  },
+  successIcon:{
+    width: 100,
+    height: 100,
+  },
+  successContainer: {
+    position: 'absolute',
+    top: 150,
+    alignSelf:"center",
+    alignItems:'center'
+  },
+  successText: {
+    color: "white",
+    lineHeight: 60,
+    fontSize: 17,
+    fontWeight: "400"
+  }
+  
+})
